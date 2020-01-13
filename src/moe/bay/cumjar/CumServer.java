ec41d0c51c0b63017171fdfbe6b13358c4ab553d
Bailey <robin.riezebos@gmail.com>
2020-01-13 21:27:44 +0100

    Create CumServer.java

---
+++ src/moe/bay/cumjar/CumServer.java
@@ -0,0 +1,24 @@
+package moe.bay.cumjar;
+
+import org.javacord.api.entity.channel.ServerTextChannel;
+import org.javacord.api.entity.server.Server;
+
+public class CumServer {
+
+    private final Server server;
+
+    public CumServer(Server server) {
+        this.server = server;
+    }
+
+    public Server getServer() {
+        return this.server;
+    }
+
+    public void setVerificationChannel(String id) {}
+
+    public ServerTextChannel getVerificationChannel() {
+
+    }
+
+}
